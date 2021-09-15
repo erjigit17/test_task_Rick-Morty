90ef1478a45da5210eaec8f69dc04a8f9661e5f2
Эржигит Имамов <erjigit17@gmail.com>
2021-09-15 09:15:15 +0600

    connect db

---
+++ database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE erjigit17 (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  body text
+);
--- index.js
+++ index.js
@@ -1,25 +1,77 @@
+'use strict';
+
 const https = require('https')
+const { Pool } = require('pg')
 
-const url = 'https://rickandmortyapi.com/api/character/'
+const PORT = process.env.PORT || 3000
+const URL = 'https://rickandmortyapi.com/api/character/'
+const TABLENAME = 'erjigit17'
 
+const pool = new Pool({
+  host: '127.0.0.1',
+  port: 5432,
+  database: 'erjigit',
+  user: 'erjigit',
+  password: '',
+})
 
-let id = 1  // 
-let stop = false; // stop flag
-
-while (!stop && id < 2 ) {
-  https.get(url + id, res => {
-    let data = '';
-    res.on('data', chunk => {
-      data += chunk;
-    });
-    res.on('end', () => {
-      data = JSON.parse(data)
-      const name = data.name
-      const obj = data
-    })
-  }).on('error', err => {
-    console.log(err.message);
-    stop = true;
+const executingСommands = (command) => {
+  pool.query(command, (err, res) => {
+    if (err) { throw err }
+    console.dir({res})
+    console.table(res.fields)
+    console.table(res.rows)
+    pool.end()
   })
-  id++;
 }
+
+const dropTable = () => {
+  executingСommands(`DROP TABLE IF EXISTS ${TABLENAME}; `)}
+
+
+
+const createTable = () => {
+  executingСommands(
+    `CREATE TABLE ${TABLENAME} (
+    id SERIAL PRIMARY KEY,
+    name text,
+    body jsonb
+  );`)
+}
+
+
+
+const insert = () => {
+  let id = 1  // 
+  let stop = false // stop flag
+
+  while (!stop && id < 2 ) {
+    https.get(URL + id, (res) => {
+      let json = ''
+      res.on('data', function (chunk) {
+          json += chunk
+      });
+      res.on('end', () => {
+        if (res.statusCode === 200) {
+          try {
+              let data = JSON.parse(json)
+              let body = JSON.stringify(data)
+              executingСommands(`INSERT INTO ${TABLENAME} (name, body)
+              VALUES('${data.name}', '${body}')`)
+          } catch (e) {
+              console.log('Error parsing JSON!')
+          }
+        } else {
+            console.log('Status:', res.statusCode)
+        }
+      })
+    }).on('error', function (err) {
+        console.log('Error:', err)
+    })
+    id++
+  }
+}
+
+insert()
+// dropTable()
+// createTable()
