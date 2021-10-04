e3d56d8767955cff6e640fc471e175f9c979b8db
Эржигит Имамов <erjigit17@gmail.com>
2021-10-04 14:22:38 +0600

    add acess to my db

--- config.js
+++ config.js
@@ -3,24 +3,24 @@
 const fs = require('fs');
 
 const config = {
-  // postgres: {
-  //   host: '127.0.0.1',
-  //   port: 5432,
-  //   database: 'erjigit',
-  //   user: 'erjigit',
-  //   password: '',
-  // },
   postgres: {
-    host: 'rc1c-2m0keqdcncuwizmx.mdb.yandexcloud.net',
-    port: 6432,
-    database: 'db1',
-    user: 'candidate',
-    password: '62I8anq3cFq5GYh2u4Lh',
-    ssl: {
-        rejectUnauthorized : false,
-        ca   : fs.readFileSync('./CA.pem').toString(),
-    },
-},
+    host: '18.192.179.9',
+    port: 5432,
+    database: 'awsDB',
+    user: 'ubuntu',
+    password: 'erji',
+  },
+  // postgres: {
+  //   host: 'rc1c-2m0keqdcncuwizmx.mdb.yandexcloud.net',
+  //   port: 6432,
+  //   database: 'db1',
+  //   user: 'candidate',
+  //   password: '62I8anq3cFq5GYh2u4Lh',
+  //   ssl: {
+  //       rejectUnauthorized : false,
+  //       ca   : fs.readFileSync('./CA.pem').toString(),
+  //   },
+  // },
 
   tableName: 'erjigit17',
 
